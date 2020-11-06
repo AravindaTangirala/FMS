92f7528da9dcfca2ef6fb18e294aafe47250a9a3
Aravinda Tangirala <atangirala@inniveinc.com>
2020-11-05 23:32:28 -0500

    third commit

---
+++ client/build/precache-manifest.13abe331d974efc78e383ec8d8b4faa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07574eff0f01f82a774de7b45eef637",
+    "url": "/feedback/index.html"
+  },
+  {
+    "revision": "7ed695ff0b41eee8e9fd",
+    "url": "/feedback/static/css/main.0bd91aad.chunk.css"
+  },
+  {
+    "revision": "890222aec3bab34cdc7e",
+    "url": "/feedback/static/js/2.f21d2377.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/feedback/static/js/2.f21d2377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed695ff0b41eee8e9fd",
+    "url": "/feedback/static/js/main.842d835a.chunk.js"
+  },
+  {
+    "revision": "d8302b9153311ab3d19b",
+    "url": "/feedback/static/js/runtime-main.389082dc.js"
+  },
+  {
+    "revision": "efda9a03e195f7b2210c344174978ab3",
+    "url": "/feedback/static/media/team.efda9a03.png"
+  }
+]);
